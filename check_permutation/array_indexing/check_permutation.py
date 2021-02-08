5229cabb759388ac20968c1253e7d9bf9301dbb9
Hafidz Fazlur Rochman <74138963+fzlur-r@users.noreply.github.com>
2021-02-08 16:01:09 +0700

    Update check_permutation.py

--- check_permutation/array_indexing/check_permutation.py
+++ check_permutation/array_indexing/check_permutation.py
@@ -6,25 +6,27 @@ def check_permutation(string1, string2):
     if length != len(string2):
         return False
     
-    #initialize array to store the character counter respective to its ascii code value
-    char_array = [0] * 256
+    #else counting how many character present in each string
+	else:
+		#initialize array to store the character counter respective to its ascii code value
+    	char_array = [0] * 256
     
-    #add the value counter to array
-    for char in string1:
-        char_array[ord(char)] += 1
+    	#add the value counter to array
+    	for char in string1:
+        	char_array[ord(char)] += 1
     
-    #substract the value counter to array
-    for char in string2:
-        #if the counter still more than 0 before substracting the its still save to continue the loop
-        if char_array[ord(char)] > 0:
-            char_array[ord(char)] -= 1
+    	#substract the value counter to array
+    	for char in string2:
+        	#if the counter still more than 0 before substracting the its still save to continue the loop
+        	if char_array[ord(char)] > 0:
+            	char_array[ord(char)] -= 1
         
-        #else the second string have more or new character which mean those string are not a permutation
-        else:
-            return False
+        	#else the second string have more or new character which mean those string are not a permutation
+        	else:
+            	return False
     
-    #if the loop finish succesfully, then those string are permutation of each other
-    return True
+    	#if the loop finish succesfully, then those string are permutation of each other
+    	return True
     
 def main():
     string1 = input()
