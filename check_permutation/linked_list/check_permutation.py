30a49473a22e2973ec7c39f96157f27f415b027f
Hafidz Fazlur Rochman <74138963+fzlur-r@users.noreply.github.com>
2021-02-08 15:49:07 +0700

    Update check_permutation.py

--- check_permutation/linked_list/check_permutation.py
+++ check_permutation/linked_list/check_permutation.py
@@ -10,7 +10,7 @@ class link:
     def __init__(self):
         self.body = None
     
-	#adding value counter to a node
+    #adding value counter to a node
     def add_val(self, value):
         if self.body is None:
             self.body = node(value)
@@ -21,7 +21,7 @@ class link:
         else:
             self.body.next = self.add_pointer(value, self.body.next)
     
-	#adding value counter to the next node
+    #adding value counter to the next node
     def add_pointer(self, value, nodes):
         if nodes is None:
             nodes = node(value)
@@ -34,7 +34,7 @@ class link:
             
         return nodes
 	
-	#substracting value counter from a node
+    #substracting value counter from a node
     def substract_val(self, value):
         if self.body is not None:
             if self.body.value == value and self.body.counter > 0:
@@ -48,7 +48,7 @@ class link:
                 if self.body.next is None:
                     self.body = None
     
-	#substracting value counter from the next node
+    #substracting value counter from the next node
     def substract_pointer(self, value, nodes):
         if nodes is not None:
             if nodes.value == value and nodes.counter > 0:
@@ -69,29 +69,29 @@ class link:
                 
 def check_permutation(string1, string2):
     #get the length of the first string
-	length = len(string1)
+    length = len(string1)
     
-	#if the first and the seconde string not in the same length, it means those string are not a permutation
+    #if the first and the seconde string not in the same length, it means those string are not a permutation
     if length != len(string2):
         return False
     
-	#else counting how many character present in each string
+    #else counting how many character present in each string
     else:
         linked_list = link()
 		
-		#first loop count the character
+        #first loop count the character
         for char in string1:
             linked_list.add_val(char)
         
-		#second loop substract the counter of character
+        #second loop substract the counter of character
         for char in string2:
             linked_list.substract_val(char)
 			
-			#if the linked list has None value in its counter (which mean lower than 0) then those string are not a permutaion
+            #if the linked list has None value in its counter (which mean lower than 0) then those string are not a permutaion
             if linked_list.body is None:
                 return False
 				
-        #if the loop have been succesfully, then those string are permutation of each other 
+        #if the loop have been succesfully, then those string are permutation of each other
         return True
 
 def main():
