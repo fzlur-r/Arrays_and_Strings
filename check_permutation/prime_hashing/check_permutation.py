d6328a89bac1a415abcbab189bfdcda2ab11e00b
Hafidz Fazlur Rochman <74138963+fzlur-r@users.noreply.github.com>
2021-02-08 16:02:25 +0700

    Update check_permutation.py

--- check_permutation/prime_hashing/check_permutation.py
+++ check_permutation/prime_hashing/check_permutation.py
@@ -6,31 +6,33 @@ def check_permutation(string1, string2):
 	if length != len(string2):
         return False
     
-    #initialize the first-16 prime number
-	prime_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
+    #else counting how many character present in each string
+	else:
+		#initialize the first-16 prime number
+		prime_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
     
-	#initialize array to count character in a string
-	char_array = [1] * 16
+		#initialize array to count character in a string
+		char_array = [1] * 16
     
-	#the first loop multiply the char_array, respective to the divisor value of char ascii code, with the prime number respective to the residual value of char ascii code.
-	for char in string1:
-        ix = ord(char)
-        char_array[ix // 16] *= prime_array[ix % 16]
+		#the first loop multiply the char_array, respective to the divisor value of char ascii code, with the prime number respective to the residual value of char ascii code.
+		for char in string1:
+        	ix = ord(char)
+        	char_array[ix // 16] *= prime_array[ix % 16]
     
-	#the second loop divide the char_array, respective to the divisor value of char ascii code, with the prime number respective to the residual value ov char ascii code.
-    for char in string2:
-        ix = ord(char)
+		#the second loop divide the char_array, respective to the divisor value of char ascii code, with the prime number respective to the residual value ov char ascii code.
+    	for char in string2:
+        	ix = ord(char)
         
-		#if the array still can be divided with the prime number then it's still save to continue the loop
-		if (char_array[ix // 16] % prime_array[ix % 16]) == 0:
-            char_array[ix // 16] /= prime_array[ix % 16]
+			#if the array still can be divided with the prime number then it's still save to continue the loop
+			if (char_array[ix // 16] % prime_array[ix % 16]) == 0:
+            	char_array[ix // 16] /= prime_array[ix % 16]
         
-		#else the second string have more or new character which mean those string are not a permutation
-		else:
-            return False
+			#else the second string have more or new character which mean those string are not a permutation
+			else:
+            	return False
     
-    #if the loop finish succesfully, then those string are permutation of each other
-	return True
+    	#if the loop finish succesfully, then those string are permutation of each other
+		return True
 	
 def main():
     string1 = input()
